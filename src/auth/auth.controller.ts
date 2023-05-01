a73dce2d3c55b261adb0cbe184347e42d0faff70
장준영 <jjy@192.168.nate.com>
2023-05-02 00:35:25 +0900

    Add(junyeong/auth/#1): 스웨거 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ import {
 import { AuthService, KakaoLogin } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { DuplicationCheckDto } from './dto/duplicationCheck.dto';
-import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -19,13 +19,14 @@ export class AuthController {
     private authService: AuthService,
     private readonly kakaologin: KakaoLogin,
   ) {}
-
+  @ApiOperation({ summary: '회원가입', description: '회원가입' })
   @Post('/signup')
   @HttpCode(200)
   singUp(@Body() authCredentialDto: AuthCredentialDto) {
     return this.authService.singUp(authCredentialDto);
   }
 
+  @ApiOperation({ summary: 'id중복체크', description: 'id를 중복체크한다.' })
   @Post('/id-duplication-ckeck')
   @HttpCode(200)
   async idDuplicationChekc(@Body() duplicationCheckDto: DuplicationCheckDto) {
@@ -38,6 +39,10 @@ export class AuthController {
     }
   }
 
+  @ApiOperation({
+    summary: '닉네임 중복체크',
+    description: '닉네임 중복체크한다.',
+  })
   @Post('/nickname-duplication-ckeck')
   @HttpCode(200)
   async nicknameDuplicationChekc(
@@ -50,39 +55,5 @@ export class AuthController {
     if (!result) {
       throw new BadRequestException('닉네임 중복');
     }
-  }
-
-  @Get('/kakaoLogin')
-  async kakaoLogin(@Res() res) {
-    const restApiKey = '8116e21560b6c70c6e6d5f0e9e1d8303'; // * 입력필요
-    // 카카오 로그인 RedirectURI 등록
-    const redirectUrl = 'http://localhost:3001/auth/kakaoLoginRedirect';
-    const url = `https://kauth.kakao.com/oauth/authorize?client_id=${restApiKey}&redirect_uri=${redirectUrl}&response_type=code`;
-    return res.redirect(url);
-  }
-
-  @Get('/kakaoLoginRedirect')
-  async kakaoLoginRedirect(@Query('code') code: string) {
-    const restApiKey = '8116e21560b6c70c6e6d5f0e9e1d8303';
-    const redirect_uri = 'http://localhost:3001/auth/kakaoLoginRedirect';
-    const hostName = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${restApiKey}&redirect_uri=${redirect_uri}&code=${code}`;
-
-    const response = await axios.post(hostName);
-    const token = response.data.access_token;
-    console.log('data');
-    console.log(response.data);
-  }
-
-  @Post('/kakaoUnlink')
-  async kakaoUnlink(@Body() data) {
-    const ACCESS_TOKEN = data.ACCESS_TOKEN;
-
-    const res = await axios.post('https://kapi.kakao.com/v1/user/unlink', {
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
-      },
-    });
-    console.log(res);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  setupSwagger(app);
+  await app.listen(3000);
 }
 
 bootstrap();
