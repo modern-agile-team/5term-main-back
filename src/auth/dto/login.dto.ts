f1e40dfafc652f798fe153c75f6a13cfe5b9298c
장준영 <jjy@192.168.nate.com>
2023-05-17 15:44:51 +0900

    Add(junyeong/auth/#1): 스웨서 로그인인api추가

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty({
+    name: 'id',
+    description: '아이디',
+    example: 'id123',
+  })
   @Matches(/^[a-zA-Z0-9]*$/, {
     message: '아이디 규격을 확인하세요',
   })
@@ -12,5 +18,10 @@ export class LoginDto {
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty({
+    name: 'password',
+    description: '비밀번호',
+    example: '1234',
+  })
   password: string;
 }
