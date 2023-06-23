f64bb65272e45ac1dc55e3c3777b09853e8c6ddc
장준영 <jjy@jangjun-yeongs-MacBook-Air.local>
2023-06-23 16:21:39 +0900

    Add(#34): 스웨거 추가

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -15,11 +15,17 @@ import { ChangePasswordDto } from 'src/auth/dto/changePassword.dto';
 import { ChangeEmailDto } from './dto/changeEmail.dto';
 import { ChangePhoneDto } from './dto/changePhone.dto';
 import { ChangeBioDto } from './dto/changeBio.dto';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private profileService: ProfileService) {}
   @Patch('/imgs')
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '프로필 사진 업로드',
+    description: '프로필 사진 업로드하는 api',
+  })
   @UseGuards(JwtAccessGuard)
   @UseInterceptors(FileInterceptor('file'))
   async uploadProfileImg(@UploadedFile() file, @GetUserId() userNo) {
@@ -27,12 +33,22 @@ export class ProfileController {
   }
 
   @Get('/')
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '프로필 가져오기',
+    description: '프로필 가져오는 api',
+  })
   @UseGuards(JwtAccessGuard)
   async getUserProfile(@GetUserId() userNo) {
     return await this.profileService.getUserProfile(userNo);
   }
 
   @Patch('/password')
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '비밀번호 변경',
+    description: '비밀번호 변경하는 api',
+  })
   @UseGuards(JwtAccessGuard)
   async changePassword(
     @GetUserId() userNo,
@@ -42,6 +58,11 @@ export class ProfileController {
   }
 
   @Patch('/email')
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '이메일 변경',
+    description: '이메일 변경하는 api',
+  })
   @UseGuards(JwtAccessGuard)
   async changeEmail(
     @GetUserId() userNo,
@@ -51,6 +72,11 @@ export class ProfileController {
   }
 
   @Patch('/phone')
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '전화번호 변경',
+    description: '전화번호 변경하는 api',
+  })
   @UseGuards(JwtAccessGuard)
   async changePhoneNumber(
     @GetUserId() userNo,
@@ -60,12 +86,22 @@ export class ProfileController {
   }
 
   @Patch('/bio')
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '자기소개 변경',
+    description: '자기소개 변경하는 api',
+  })
   @UseGuards(JwtAccessGuard)
   async changeBio(@GetUserId() userNo, @Body() changeBioDto: ChangeBioDto) {
     return this.profileService.changeBio(userNo, changeBioDto);
   }
 
   @Get('/my-posts')
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '내가 쓴 글 가져오기',
+    description: '내가 쓴 글 가져오는 api',
+  })
   @UseGuards(JwtAccessGuard)
   async getMyPosts(@GetUserId() userNo) {
     return this.profileService.getMyPosts(userNo);
