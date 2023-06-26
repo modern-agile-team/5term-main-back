bb834df44d48f425c4ac8e76c923dfb095d23461
Rnut3037 <pso303702@naver.com>
2023-06-26 13:34:54 +0900

    Fix(#7) : 스터디 모집글 엔티티 수정

--- src/study_recruit/entities/study_recruit_boardIMG.entity.ts
+++ src/study_recruit/entities/study_recruit_boardIMG.entity.ts
@@ -1,4 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Study } from 'src/study/entities/study.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity({
   name: 'study_recruit_board_img',
@@ -12,4 +20,8 @@ export class RecruitboardIMG extends BaseEntity {
 
   @Column()
   imgKey: string;
+
+  @OneToOne(() => Study)
+  @JoinColumn({ name: 'study_id' })
+  studyId: Study;
 }
